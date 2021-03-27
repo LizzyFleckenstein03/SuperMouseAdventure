f0791ef510cf96a59a162091d71006e386722ee1
KG0104 <76601263+KG0104@users.noreply.github.com>
2021-03-27 08:07:55 +0100

    Harmlose Gegner
    Es gibt jetzt Gegner, die den Spieler verfolgen, aber keinen schaden anrichten

---
+++ 2DGame/Assets/Scripts/Gegner/FollowPlayer.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //SerializeFields sind private, tauchen allerdings trotzdem im Inspector in Unity auf
+
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float agroRange;
+
+    [SerializeField]
+    float MoveSpeed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Abstand zur Spielfigur
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(distToPlayer < agroRange)
+        {
+            ChasePlayer();
+        } else
+        {
+            StopChasingPlayer();
+        }
+
+    }
+
+    void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            //Der Gegner ist links neben dem Spieler, also bewegt er sich nach rechts
+            rb.velocity = new Vector2(MoveSpeed, 0);
+
+            //Gegner wird der Bewegung entsprechend gedreht
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if(transform.position.x > player.position.x)
+        {
+            //Der Gegner ist rechts neben dem Spieler, also bewegt er sich nach links
+            rb.velocity = new Vector2(-MoveSpeed, 0);
+
+            //Gegner wird der Bewegung entsprechend gedreht
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    void StopChasingPlayer()
+    {
+        //Geschwindigkeit des Gegners auf null setzen
+        rb.velocity = Vector2.zero;
+    }
+}
