afa6c13a70c0a05df00c361c309b3eb73329e2ef
KG0104 <76601263+KG0104@users.noreply.github.com>
2021-03-30 08:49:41 +0200

    Herzen
    Wenn die Maus die Gegner berührt, wird ihr ein Herz abgezogen.

---
+++ 2DGame/Assets/Scripts/Maus/Health.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int mouseHealth;
+    public int numberOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite empytyHeart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mouseHealth > numberOfHearts)
+        {
+            mouseHealth = numberOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            //Wenn i kleiner als die Gesundheit, zeige ein volles Herz an, ansonsten ein leeres
+            if(i < mouseHealth)
+            {
+                hearts[i].sprite = fullHeart;
+            } else
+            {
+                hearts[i].sprite = empytyHeart;
+            }
+
+            if(i < numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            } else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    //Bei Berührung mit einem Gegner wird die Gesundheit um 1 verringert
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy"))
+        {
+            mouseHealth--;
+        }
+    }
+}
