a762f27bf630ab8409e88abe2930ac2a4b92a3bc
KG0104 <76601263+KG0104@users.noreply.github.com>
2021-08-25 11:43:19 +0200

    Level-Auswahl v0.3

---
+++ 2DGame/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] levels;
+
+    int[] scenes;
+
+    private int level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = 0;
+        transform.position = levels[level].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            if (level > 0)
+            {
+                level -= 1;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (level < 9)
+            {
+                level += 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(level + 1);
+        }
+
+        transform.position = levels[level].transform.position;
+    }
+}
